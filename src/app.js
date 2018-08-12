83c24652642b9f71e5b5c0fded415606f5e90af1
Carlos G <cguidog@gmail.com>
2018-08-12 16:20:27 -0400

    Minor fixes

--- src/app.js
+++ src/app.js
@@ -10,12 +10,12 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addProject({name: '', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'html'}))
-store.dispatch(addProject({name: '', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'css'}))
-store.dispatch(addProject({name: 'React Car Search', thumbnail: '/resources/react_car_search.png', git: 'https://github.com/cguidog/carDisplay', description: 'Enter text here', link: 'https://react-car-search.herokuapp.com', tool:'react'}))
-store.dispatch(addProject({name: 'RGB Color Guessing Game', thumbnail: '/resources/rgb_game.png', git: '', description: 'Enter text here', link: '', tool:'javascript'}))
-store.dispatch(addProject({name: '', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'jquery'}))
-store.dispatch(addProject({name: '', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'jquery'}))
+store.dispatch(addProject({name: 'ToDo List', thumbnail: '/resources/to_do_list.png', git: 'https://github.com/cguidog/to-do-list', description: 'A simple to-do app created with Jquery', link: 'https://cguidog.github.io/to-do-list/index.html', tool:'Jquery'}))
+store.dispatch(addProject({name: 'Coming Soon', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'css'}))
+store.dispatch(addProject({name: 'React Car Search', thumbnail: '/resources/react_car_search.png', git: 'https://github.com/cguidog/carDisplay', description: 'Enter text here', link: 'https://react-car-search.herokuapp.com', tool:'React'}))
+store.dispatch(addProject({name: 'RGB Color Guessing Game', thumbnail: '/resources/rgb_game.png', git: 'https://github.com/cguidog/rgb-color-game', description: 'Test your RGB knowledge picking the right color', link: 'https://cguidog.github.io/rgb-color-game/', tool:'Javascript'}))
+store.dispatch(addProject({name: 'Circles', thumbnail: '/resources/circles.png', git: 'https://github.com/cguidog/circles', description: 'Random circles generator built with Paper.js', link: 'https://cguidog.github.io/circles/index.html', tool:'Javascript'}))
+store.dispatch(addProject({name: 'Coming Soon', thumbnail: '/resources/coming_soon.png', git: '', description: 'Enter text here', link: '', tool:'Jquery'}))
 
 
 const jsx = (
--- src/components/ProjectListItem.js
+++ src/components/ProjectListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGift } from '@fortawesome/free-solid-svg-icons';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+
 
 const ProjectListItem = ({name, thumbnail, description, link, git}) => (
     <div>
-    <img src={thumbnail}/>
-    <h4>{name}</h4>
+    <h4><a href={link} target='_blank'>{name}</a> <a href={git} target='_blank'><span><FontAwesomeIcon icon={faGithubSquare}/></span></a></h4>
+    <a href={link} target='_blank'><img width='300px' src={thumbnail}/></a>
     <p>{description}</p>
-    <a href={link}>Visit</a>
-    <a href={git}>GitHub</a>
     </div>
 );
 
