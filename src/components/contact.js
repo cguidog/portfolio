7f80642349557ad9915fa0c4a2c5881ed2e1ed7a
Carlos G <cguidog@gmail.com>
2018-08-25 11:56:47 -0400

    Minor changes

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,7 @@ const Contact = () => (
     <div>
         <h2 className='titles'>Contact</h2>
         <div id='contact' className='links'>
-        <a href='' target='_blank'><FontAwesomeIcon icon={faLinkedinIn}/></a>
+        <a href='https://www.linkedin.com/in/carlos-guido-142998156/' target='_blank'><FontAwesomeIcon icon={faLinkedinIn}/></a>
         <a href='https://github.com/cguidog' target='_blank'><FontAwesomeIcon icon={faGithub}/></a>
         </div>
     </div>
