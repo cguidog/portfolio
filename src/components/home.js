484d399ac583657cad9d25d03d8bcecdf376bf11
Carlos G <cguidog@gmail.com>
2018-08-25 08:43:04 -0400

    Adjusted responsive css

--- src/components/home.js
+++ src/components/home.js
@@ -6,13 +6,13 @@ import { faCode } from '@fortawesome/free-solid-svg-icons'
 const Home = () => (
     <div>
         <div id='welcome'>
-            <h1 className='head'>{'<Carlos Guido Guido/>'}</h1>
+            <h1 className='head'>{'<CarlosGuidoGuido/>'}</h1>
             <h3 id='sub'>Front-end developer</h3>
             <div className='text_box'>
             <p id='about'>Passionate professional who loves to create: from building furniture to designing clothes, to tiling kitchens to writing code.</p>
             <p id='tech'>I build responsive web applications using HTML, CSS and Javascript. I have experience working with Javascript libraries like ReactJS and Jquery
                 and enjoy giving style to my creations with Sass and Bootstrap.</p>
-            <span className='random'><FontAwesomeIcon icon={faCode} /></span>
+            <span className='animat'><FontAwesomeIcon icon={faCode} /></span>
             </div>
         </div>
     </div>
