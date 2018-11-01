3938f099e14e5db4f87ab41135da6e496187768e
Carlos G <cguidog@gmail.com>
2018-11-01 13:19:52 -0400

    Updated link to portfolio projects

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ store.dispatch(addProject({name: 'Portfolio Site', thumbnail: '/resources/portfo
 store.dispatch(addProject({name: 'React Car Search', thumbnail: '/resources/react_car_search.png', git: 'https://github.com/cguidog/carDisplay', description: 'Simple search tool created with React.', link: 'https://react-car-search.herokuapp.com', tool:'React'}))
 store.dispatch(addProject({name: 'RGB Color Guessing Game', thumbnail: '/resources/rgb_game.png', git: 'https://github.com/cguidog/rgb-color-game', description: 'Test your RGB knowledge picking the right color.', link: 'https://cguidog.github.io/rgb-color-game/', tool:'JavaScript'}))
 store.dispatch(addProject({name: 'ToDo List', thumbnail: '/resources/to_do_list.png', git: 'https://github.com/cguidog/to-do-list', description: 'A simple to-do app created with Jquery.', link: 'https://cguidog.github.io/to-do-list/index.html', tool:'Jquery'}))
-store.dispatch(addProject({name: 'Weather App', thumbnail: '/resources/weather_app.png', git: 'https://github.com/cguidog/weather-app', description: 'App created with Javascript to show local weather.', link: 'https://codepen.io/cguidog/full/VQvKbW/', tool:'JavaScript'}))
+store.dispatch(addProject({name: 'Weather App', thumbnail: '/resources/weather_app.png', git: 'https://github.com/cguidog/weather-app', description: 'App created with Javascript to show local weather.', link: 'https://codepen.io/cguidog/full/LJPZry/', tool:'JavaScript'}))
 store.dispatch(addProject({name: 'Wikipedia Viewer', thumbnail: '/resources/wiki_viewer.png', git: 'https://github.com/cguidog/wikipediaViewer', description: 'Display 5 main results from a Wikipedia Search.', link: 'https://cguidog.github.io/wikipediaViewer/', tool:'Jquery'}))
 
 
